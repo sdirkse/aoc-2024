0373466228b382517120cc60680a2e7260e9d637
Stefan Dirkse <sdirkse@gmail.com>
2024-12-05 13:04:08 +0100

    Update to class template to prevent \r errors in input

--- src/main/resources/day-class-template.java
+++ src/main/resources/day-class-template.java
@@ -3,13 +3,13 @@ package com.github.sdirkse;
 class DayDAY_NUMBER {
 
     long partOne(String input) {
-        String[] lines = input.split("\n");
+        String[] lines = input.split("\r?\n");
 
         return 0;
     }
 
     long partTwo(String input) {
-        String[] lines = input.split("\n");
+        String[] lines = input.split("\r?\n");
 
         return 0;
     }
