118447927de77df1b39f0205597fa872b10c266f
Stefan Dirkse <sdirkse@gmail.com>
2024-12-16 07:10:30 +0100

    Extended GridUtils

--- src/main/java/com/github/sdirkse/utils/GridUtils.java
+++ src/main/java/com/github/sdirkse/utils/GridUtils.java
@@ -5,15 +5,25 @@ import java.util.Arrays;
 public class GridUtils {
 
 
-    public static char[][] createGrid(int width, int length) {
-        return new char[width][length];
+    public static char[][] createGrid(int height, int length) {
+        return new char[height][length];
     }
 
-    public static char[][] createGrid(int width, int length, char defaultChar) {
-        char[][] grid = createGrid(width, length);
+    public static char[][] createGrid(int height, int length, char defaultChar) {
+        char[][] grid = createGrid(height, length);
 
         for (char[] row : grid) {
             Arrays.fill(row, defaultChar);
+        }
+
+        return grid;
+    }
+
+    public static int[][] createIntGrid(int height, int length, int defaultNumber) {
+        int[][] grid = new int[height][length];
+
+        for (int[] row : grid) {
+            Arrays.fill(row, defaultNumber);
         }
 
         return grid;
@@ -23,6 +33,15 @@ public class GridUtils {
         for (char[] row : grid) {
             for (char c : row) {
                 System.out.printf("%s", c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void drawIntGrid(int[][] grid) {
+        for (int[] row : grid) {
+            for (int n : row) {
+                System.out.printf("%s", n);
             }
             System.out.println();
         }
